9b8b9b44f30e9a277ff0fa6b4c644a48b77e4bcd
KMConner <KMConner@users.noreply.github.com>
2020-09-11 01:36:03 +0900

    Write passed strings to stdout

--- PipeClient/PipeClient.cpp
+++ PipeClient/PipeClient.cpp
@@ -1,1 +1,8 @@
-﻿
+﻿#include "PipeClient.h"
+#include <stdio.h>
+
+BOOL Send(TCHAR* content, TCHAR* pipeName)
+{
+	wprintf(content);
+	return TRUE;
+}
---
+++ PipeClient/PipeClient.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <Windows.h>
+
+BOOL Send(TCHAR* content, TCHAR* pipeName);
