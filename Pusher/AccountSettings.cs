fcf2ec791a760fa1ffcaafcb641997e926241300
KMConner <KMConner@users.noreply.github.com>
2020-09-17 01:11:50 +0900

    Created configuration schema

---
+++ Pusher/AccountSettings.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization;
+
+namespace Pusher
+{
+    [DataContract]
+    class AccountSettings
+    {
+        [DataMember(Name = "consumer-key")]
+        public string ConsumerKey { get; set; }
+
+        [DataMember(Name = "consumer-key-secret")]
+        public string ConsumerKeySecret { get; set; }
+
+        [DataMember(Name = "access-token")]
+        public string AccessToken { get; set; }
+
+        [DataMember(Name = "access-token-secret")]
+        public string AccessTokenSecret { get; set; }
+
+        public bool Validate()
+        {
+            if (string.IsNullOrEmpty(ConsumerKey) || string.IsNullOrEmpty(ConsumerKeySecret)
+                || string.IsNullOrEmpty(AccessToken) || string.IsNullOrEmpty(AccessTokenSecret))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
