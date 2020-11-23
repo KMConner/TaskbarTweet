1a9475926c9c25a6004e0d1eb9b40df5e3170253
KMConner <KMConner@users.noreply.github.com>
2020-11-23 23:47:30 +0900

    Set data binding

--- Setup/MainWindow.xaml.cs
+++ Setup/MainWindow.xaml.cs
@@ -1,28 +1,110 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System;
+using MvvmCross.Commands;
 
 namespace Setup
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string apiKey;
+
+        private string apiSecret;
+
+        private string accessToken;
+
+        private string accessTokenSecret;
+
+        private IMvxCommand cancelCommand;
+
+        private IMvxCommand okCommand;
+
+        public string ApiKey
+        {
+            get { return apiKey; }
+            set
+            {
+                apiKey = value;
+                NotifyProperyChanged(nameof(ApiKey));
+                OkCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public string ApiSecret
+        {
+            get { return apiSecret; }
+            set
+            {
+                apiSecret = value;
+                NotifyProperyChanged(nameof(ApiSecret));
+                OkCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public string AccessToken
+        {
+            get { return accessToken; }
+            set
+            {
+                accessToken = value;
+                NotifyProperyChanged(nameof(AccessToken));
+                OkCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public string AccessTokenSecret
+        {
+            get { return accessTokenSecret; }
+            set
+            {
+                accessTokenSecret = value;
+                NotifyProperyChanged(nameof(AccessTokenSecret));
+                OkCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public IMvxCommand CancelCommand
+        {
+            get => cancelCommand ??= new MvxCommand(Close);
+        }
+
+        public IMvxCommand OkCommand
+        {
+            get => okCommand ??= new MvxCommand(OkCommandExecute, OkCommandCanExecute);
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        private void NotifyProperyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void OkCommandExecute()
+        {
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            Close();
+        }
+
+        private bool OkCommandCanExecute()
+        {
+            return !string.IsNullOrWhiteSpace(apiKey) && !string.IsNullOrWhiteSpace(ApiSecret) &&
+                !string.IsNullOrWhiteSpace(AccessToken) && !string.IsNullOrWhiteSpace(AccessTokenSecret);
+        }
+
+        private void SaveToIni()
+        {
+
         }
     }
 }
